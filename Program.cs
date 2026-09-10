eb6ce06977e4da02c64c3bb1e0b19736214d14ad
rosangela <tomaziaalcantara5@gmail.com>
2026-09-09 21:19:51 -0300

    Initial project upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+class ImitandoDory
+{
+    static string TraduzirParaBaleies(string texto)
+    {
+        string vogais = "aeiouAEIOUáéíóúÁÉÍÓÚâêîôûÂÊÎÔÛãõÃÕ";
+        StringBuilder resultado = new StringBuilder();
+
+        foreach (char letra in texto)
+        {
+            if (vogais.IndexOf(letra) >= 0)
+            {
+                resultado.Append(letra, 3);
+            }
+            else
+            {
+                resultado.Append(letra);
+            }
+        }
+
+        return resultado.ToString();
+    }
+
+    static void Main()
+    {
+        Console.Write("Digite o texto: ");
+        string texto = Console.ReadLine() ?? "";
+
+        string traduzido = TraduzirParaBaleies(texto);
+        Console.WriteLine(traduzido);
+    }
+}
